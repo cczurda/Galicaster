bb9df4da843543fd88bc841d69a6567b017beaac
cczurda <christoph.czurda@gmail.com>
2013-07-07 12:55:17 +0200

    visca.recall can move camera to predefined position

--- galicaster/utils/visca.py
+++ galicaster/utils/visca.py
@@ -1,14 +1,7 @@
-# call 'visca-cli memory_recall [0-5]'
-# we don't check if daemon is running, we don't check if client is installed
-#
+# executes 'visca-cli memory_recall [0-5]'
+# we don't check if daemon is running or if client is installed
 
-import subprocess
-
-#from galicaster.core import context
-
-#logger = context.get_logger()
+from subprocess import call
 
 def recall(preset):
-    #logger.info("recall visca camera preset %d", preset)
-    subprocess.call("visca-cli", "memory_recall", preset) #????
-
+    call(['visca-cli', 'memory_recall', str(preset)])
