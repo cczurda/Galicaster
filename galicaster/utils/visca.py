74d61af16d2e20f0584b90c27baf36c247b37e71
cczurda <christoph.czurda@gmail.com>
2013-07-07 14:22:46 +0200

    using subprocess.Popen instead of call for non-blocking behaviour

--- galicaster/utils/visca.py
+++ galicaster/utils/visca.py
@@ -1,7 +1,7 @@
 # executes 'visca-cli memory_recall [0-5]'
 # we don't check if daemon is running or if client is installed
 
-from subprocess import call
+from subprocess import Popen
 
 def recall(preset):
-    call(['visca-cli', 'memory_recall', str(preset)])
+    Popen(['visca-cli', 'memory_recall', str(preset)])
